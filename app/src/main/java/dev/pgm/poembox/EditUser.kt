5c997e3fbd551c3df12644ed51f4aaef4a28de97
PedroGM80 <pedro13087@gmail.com>
2022-10-22 18:05:34 +0200

    add Edit user and update User open class

---
+++ app/src/main/java/dev/pgm/poembox/EditUser.kt
@@ -0,0 +1,24 @@
+package dev.pgm.poembox
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class EditUser(val poemEdited: PoemDraft, userName: String, Id: Int, mail: Int) :
+    User(userName, Id, mail) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun submitPoem(title: String, draftContent: List<String>) {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        val current = LocalDateTime.now().format(formatter)
+        val poemEdited = PoemDraft(
+            1, title,
+            draftContent,
+            current,
+            null
+        )
+
+    }
+
+}
