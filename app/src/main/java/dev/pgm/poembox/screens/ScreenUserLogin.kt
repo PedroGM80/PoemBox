c638010df2460f1f43199cef453e9987665aabd0
PedroGM80 <pedro13087@gmail.com>
2022-10-30 20:27:54 +0100

    add user login

---
+++ app/src/main/java/dev/pgm/poembox/screens/ScreenUserLogin.kt
@@ -0,0 +1,107 @@
+package dev.pgm.poembox.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import dev.pgm.poembox.ui.theme.Purple700
+import kotlinx.coroutines.delay
+
+@Composable
+fun UserLogin(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        ClickableText(
+            text = AnnotatedString("Sign up here"),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(20.dp),
+            onClick = { },
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default,
+                textDecoration = TextDecoration.Underline,
+                color = Purple700
+            )
+        )
+    }
+    Column(
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        val username = remember { mutableStateOf(TextFieldValue()) }
+        val eMail = remember { mutableStateOf(TextFieldValue()) }
+
+        Text(text = "Login", style = TextStyle(fontSize = 40.sp, fontFamily = FontFamily.Cursive))
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Username") },
+            value = username.value,
+            onValueChange = { username.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Email") },
+            value = eMail.value,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            onValueChange = { eMail.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = {
+                suspend {
+                        delay(100)
+                        navController.navigate(ScreensRouteList.RouteScreenTabs.route) {
+                            popUpTo(0)
+                        }
+                    }
+                },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Login")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        ClickableText(
+            text = AnnotatedString("Forgot password?"),
+            onClick = { },
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrevScreenUserLogin() {
+    UserLogin(rememberNavController())
+}
--- app/src/main/java/dev/pgm/poembox/screens/ScreensRouteList.kt
+++ app/src/main/java/dev/pgm/poembox/screens/ScreensRouteList.kt
@@ -3,4 +3,5 @@ package dev.pgm.poembox.screens
 sealed class ScreensRouteList(val route:String){
     object RouteScreenSplash: ScreensRouteList("S")
     object RouteScreenTabs: ScreensRouteList("T")
+   object RouteScreenLogin: ScreensRouteList("L")
 }
