f250cd5f6312108d20d7cee50bc633b0ffb1fb0b
PedroGM80 <pedro13087@gmail.com>
2022-11-01 16:56:00 +0100

    try fix database with room

--- app/src/main/java/dev/pgm/poembox/PoemDraft.kt
+++ app/src/main/java/dev/pgm/poembox/PoemDraft.kt
@@ -1,8 +1,15 @@
 package dev.pgm.poembox
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class PoemDraft(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
     val title: String,
     val draftContent: String,
     val draftAnnotation: String,
-    val writer: User
+    val writer: User,
+    val date:String
 )
