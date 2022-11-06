4a448aec8ac93d7d8f3c589a54d90e276a143d31
PedroGM80 <pedro13087@gmail.com>
2022-11-06 13:40:58 +0100

    room try 2

--- app/src/main/java/dev/pgm/poembox/roomUtils/PoemDraft.kt
+++ app/src/main/java/dev/pgm/poembox/roomUtils/PoemDraft.kt
@@ -1,16 +1,19 @@
 package dev.pgm.poembox.roomUtils
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import dev.pgm.poembox.User
+import java.util.Date
 
 @Entity
 data class PoemDraft(
-    @PrimaryKey(autoGenerate = true)
-    val id:Int,
-    val title: String,
-    val draftContent: String,
-    val draftAnnotation: String,
-    val writer: User,
-    val date:String
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id") val id:Int,
+    @ColumnInfo(name = "title")val title: String,
+    @ColumnInfo(name = "draftContent")val draftContent: String,
+    @ColumnInfo(name = "draftAnnotation")val draftAnnotation: String,
+    @ColumnInfo(name = "writerName")val writerName:String,
+    @ColumnInfo(name = "WrittenDate")val WrittenDate:String?="CURRENT_TIMESTAMP",
+    @ColumnInfo(name = "dateValidation")val dateValidation:String?=null
 )
