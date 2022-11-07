9be1447644060d31b2cff593efa02b95499393d0
PedroGM80 <pedro13087@gmail.com>
2022-11-07 22:08:36 +0100

    room try 3

--- app/src/main/java/dev/pgm/poembox/roomUtils/PoemDraft.kt
+++ app/src/main/java/dev/pgm/poembox/roomUtils/PoemDraft.kt
@@ -4,16 +4,16 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import dev.pgm.poembox.User
+import org.jetbrains.annotations.NotNull
+import java.math.BigInteger
 import java.util.Date
-
 @Entity
 data class PoemDraft(
-    @PrimaryKey(autoGenerate = false)
-    @ColumnInfo(name = "id") val id:Int,
-    @ColumnInfo(name = "title")val title: String,
-    @ColumnInfo(name = "draftContent")val draftContent: String,
-    @ColumnInfo(name = "draftAnnotation")val draftAnnotation: String,
-    @ColumnInfo(name = "writerName")val writerName:String,
-    @ColumnInfo(name = "WrittenDate")val WrittenDate:String?="CURRENT_TIMESTAMP",
-    @ColumnInfo(name = "dateValidation")val dateValidation:String?=null
+    @PrimaryKey val id:Int?,
+    @ColumnInfo(name = "title")val title: String?,
+    @ColumnInfo(name = "draftContent")val draftContent: String?,
+    @ColumnInfo(name = "draftAnnotation")val draftAnnotation: String?,
+ //   @ColumnInfo(name = "writerName")val writerName:String,
+   // @ColumnInfo(name = "WrittenDate")val WrittenDate:String?="CURRENT_TIMESTAMP",
+   // @ColumnInfo(name = "dateValidation")val dateValidation:String?=null
 )
