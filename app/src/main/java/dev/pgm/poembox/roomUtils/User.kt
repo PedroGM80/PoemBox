280d8c55048deae97df91da59b23fba0f80756ec
PedroGM80 <pedro13087@gmail.com>
2022-12-04 21:51:23 +0100

    user Login user Register config update

--- app/src/main/java/dev/pgm/poembox/roomUtils/User.kt
+++ app/src/main/java/dev/pgm/poembox/roomUtils/User.kt
@@ -3,13 +3,14 @@ package dev.pgm.poembox.roomUtils
 import android.os.Build
 import androidx.annotation.RequiresApi
 import dev.pgm.poembox.MainActivity
+import dev.pgm.poembox.MainActivity.Companion.USER_DATA
 
 
 @RequiresApi(Build.VERSION_CODES.M)
 class User(var userName: String?, var mail: String?) {
     @RequiresApi(Build.VERSION_CODES.M)
     fun existUserRegister(): Boolean {
-        return MainActivity().getUser().isNotEmpty()
+       return USER_DATA.isNotEmpty()
     }
 
 }
