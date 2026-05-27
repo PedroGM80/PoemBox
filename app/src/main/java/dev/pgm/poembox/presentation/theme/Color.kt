53b101a3732d1a9eb64952814cb4a311906bcb18
pedrogallegomorales <pedro13087@gmail.com>
2026-05-27 11:26:02 +0200

    UI: Refine color palette for a more poetic atmosphere

--- app/src/main/java/dev/pgm/poembox/presentation/theme/Color.kt
+++ app/src/main/java/dev/pgm/poembox/presentation/theme/Color.kt
@@ -2,34 +2,34 @@ package dev.pgm.poembox.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_primary = Color(0xFF4E7BD5)
+val md_theme_light_primary = Color(0xFF904A42) // Deep Garnet
 val md_theme_light_onPrimary = Color.White
-val md_theme_light_primaryContainer = Color(0xFFDCE6FF)
-val md_theme_light_onPrimaryContainer = Color(0xFF001B4F)
-val md_theme_light_secondary = Color(0xFF00A48A)
+val md_theme_light_primaryContainer = Color(0xFFFFDAD5)
+val md_theme_light_onPrimaryContainer = Color(0xFF3B0906)
+val md_theme_light_secondary = Color(0xFF006A60) // Dark Emerald
 val md_theme_light_onSecondary = Color.White
-val md_theme_light_secondaryContainer = Color(0xFFA4F2DE)
-val md_theme_light_onSecondaryContainer = Color(0xFF00201A)
-val md_theme_light_background = Color(0xFFFBF9FE)
-val md_theme_light_onBackground = Color(0xFF1C1B1F)
-val md_theme_light_surface = Color(0xFFFBF9FE)
-val md_theme_light_onSurface = Color(0xFF1C1B1F)
+val md_theme_light_secondaryContainer = Color(0xFF74F8E5)
+val md_theme_light_onSecondaryContainer = Color(0xFF00201C)
+val md_theme_light_background = Color(0xFFFFF8F1) // Warm Paper
+val md_theme_light_onBackground = Color(0xFF201A19) // Inky Black
+val md_theme_light_surface = Color(0xFFFFF8F1)
+val md_theme_light_onSurface = Color(0xFF201A19)
 
-val md_theme_dark_primary = Color(0xFFB1C5FF)
-val md_theme_dark_onPrimary = Color(0xFF003063)
-val md_theme_dark_primaryContainer = Color(0xFF274D9F)
-val md_theme_dark_onPrimaryContainer = Color(0xFFDCE6FF)
-val md_theme_dark_secondary = Color(0xFF55E2C4)
-val md_theme_dark_onSecondary = Color(0xFF00382F)
-val md_theme_dark_secondaryContainer = Color(0xFF006B55)
-val md_theme_dark_onSecondaryContainer = Color(0xFFA4F2DE)
-val md_theme_dark_background = Color(0xFF1C1B1F)
-val md_theme_dark_onBackground = Color(0xFFE6E1E5)
-val md_theme_dark_surface = Color(0xFF1C1B1F)
-val md_theme_dark_onSurface = Color(0xFFE6E1E5)
+val md_theme_dark_primary = Color(0xFFFFB4AA)
+val md_theme_dark_onPrimary = Color(0xFF561E18)
+val md_theme_dark_primaryContainer = Color(0xFF73332C)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFDAD5)
+val md_theme_dark_secondary = Color(0xFF53DBC9)
+val md_theme_dark_onSecondary = Color(0xFF003731)
+val md_theme_dark_secondaryContainer = Color(0xFF005045)
+val md_theme_dark_onSecondaryContainer = Color(0xFF74F8E5)
+val md_theme_dark_background = Color(0xFF1A1C1E) // Deep Midnight
+val md_theme_dark_onBackground = Color(0xFFE2E2E6)
+val md_theme_dark_surface = Color(0xFF1A1C1E)
+val md_theme_dark_onSurface = Color(0xFFE2E2E6)
 
-val ImmersiveDarkBackground = Color(0xFF1A1A2E)
-val ImmersiveDarkText = Color(0xFFF5F0E8)
-val ImmersiveWarmBackground = Color(0xFFFAF3E0)
-val ImmersiveWarmText = Color(0xFF3E2723)
+val PaperBackground = Color(0xFFFAF3E0)
+val InkText = Color(0xFF2C2421)
+val MidnightBackground = Color(0xFF121212)
+val StarlightText = Color(0xFFE0E0E0)
 
